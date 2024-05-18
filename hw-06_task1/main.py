310f7d764d964078597476a351f3042495b572b8
svitlanakl <sv.klepka@gmail.com>
2024-05-18 18:31:55 +0300

    draft reviewed

--- hw-06_task1/main.py
+++ hw-06_task1/main.py
@@ -16,11 +16,24 @@ class Name(Field):
 class Phone(Field):
     # реалізація класу
 		pass
+# валідація номеру  перевірка на 10 цифр
 
 class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
+    
+    def add_phone(self):
+        pass
+    
+    def remove_phone(self):
+        pass
+
+    def find_phone(self):
+        pass
+
+    def edit_phone(self):
+        pass
 
     # реалізація класу
 
@@ -29,4 +42,45 @@ class Record:
 
 class AddressBook(UserDict):
     # реалізація класу
-		pass
+    def add_record(self):
+        pass
+    
+    def find(self):
+        pass
+    
+    def delete(self):
+        pass
+
+if __name__ == '__main__':
+        # Створення нової адресної книги
+    book = AddressBook()
+
+    # Створення запису для John
+    john_record = Record("John")
+    john_record.add_phone("1234567890")
+    john_record.add_phone("5555555555")
+
+    # Додавання запису John до адресної книги
+    book.add_record(john_record)
+
+    # Створення та додавання нового запису для Jane
+    jane_record = Record("Jane")
+    jane_record.add_phone("9876543210")
+    book.add_record(jane_record)
+
+    # Виведення всіх записів у книзі
+    for name, record in book.data.items():
+        print(record)
+
+    # Знаходження та редагування телефону для John
+    john = book.find("John")
+    john.edit_phone("1234567890", "1112223333")
+
+    print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+    # Пошук конкретного телефону у записі John
+    found_phone = john.find_phone("5555555555")
+    print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
+    # Видалення запису Jane
+    book.delete("Jane")
